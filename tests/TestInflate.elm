fb38370a0d108e41c5ffe5ec085b96c057bf9558
Folkert <folkert@folkertdev.nl>
2019-07-04 11:55:55 +0200

    fix unsigned 32bit number issue

--- tests/TestInflate.elm
+++ tests/TestInflate.elm
@@ -19,6 +19,12 @@ import TestData.Lorem as Lorem
 import ZLib
 
 
+fuzzBytes : Fuzz.Fuzzer Bytes
+fuzzBytes =
+    Fuzz.list (Fuzz.map Encode.unsignedInt8 (Fuzz.intRange 0 255))
+        |> Fuzz.map (Encode.sequence >> Encode.encode)
+
+
 exactly n decoder =
     let
         go ( i, accum ) =
@@ -61,6 +67,10 @@ adler32 =
         , setup 196611 [ 2 ]
         , setup 88473791 [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 ]
         , setup 1604860569 (List.concat (List.repeat 1000 [ 1, 2, 3, 4, 5 ]))
+        , fuzz fuzzBytes "adler32 output is always > 0" <|
+            \buffer ->
+                Adler32.adler32 buffer
+                    |> Expect.greaterThan 0
         ]
 
 
@@ -77,6 +87,10 @@ crc32 =
     describe "crc32 checksum"
         [ setup 2768625435 [ 1 ]
         , setup 1007455905 [ 2 ]
+        , fuzz fuzzBytes "crc32 output is always > 0" <|
+            \buffer ->
+                Crc32.crc32 buffer
+                    |> Expect.greaterThan 0
         ]
 
 
